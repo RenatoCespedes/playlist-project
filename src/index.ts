050164bba7fc29ae61a1734c012b4dd1f8710808
renato <renatocespedesf@gmail.com>
2023-01-21 18:35:31 -0500

    archive modified to listen a port

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-import express, {Express, Request, Response} from 'express';
-import * as ROUTER from "./components";
+import app from "./app";
+const port = 9001;
 
-const app: Express = express();
-
-
-app.use(express.json())
-
-//app.use("/api/v1/users", ROUTER.userRouter);
-app.use("/api/v1/songs", ROUTER.songrouter);
-app.use("/api/v1/playlist ", ROUTER.playlistRouter);
-
-const port = 3000;
 app.listen(port, ()=> {
-console.log(`[Server]: I am running at https://localhost:${port}`);
+console.log(`[Server]: I am running at http://localhost:${port}`);
 });

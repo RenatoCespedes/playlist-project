694950a2ec2dc4c49a2b9386a6bb212cfd8c827d
renato <renatocespedesf@gmail.com>
2023-01-21 18:29:08 -0500

    configuration of routes for the new method

--- src/components/playlist/index.ts
+++ src/components/playlist/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { findAll,createPlaylist } from "./controller";
+import { findAll,createPlaylist,add } from "./controller";
 
 const playlistRouter: Router = Router();
 
 playlistRouter.get("/", findAll);
 playlistRouter.post("/",createPlaylist);
+playlistRouter.put("/",add);
 
 export default playlistRouter;

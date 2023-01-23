702a1032e1750b3a9861bc47d742572a24d35dc2
Aljes2310 <aljes2310e@gmail.com>
2023-01-22 23:53:31 -0500

    comparacion en login de password(request) y password hasheada (db)

--- src/components/songs/controller.ts
+++ src/components/songs/controller.ts
@@ -5,7 +5,6 @@ import  Prueba from "./interface";
 
 
 const prisma= new PrismaClient();
-const user={email: "json", password:"webtoken"}
 
 export const findAll = async (_req: Request, res: Response): Promise<void> => {
 
@@ -15,8 +14,13 @@ export const findAll = async (_req: Request, res: Response): Promise<void> => {
     if(token) {
 
         const prueba =  jwt.verify(token.split(" ")[1], "tokensecret") as Prueba;
-        
-        if (user.email == prueba.email) {
+        const existingUser = await prisma.user.findFirst({ //findFirst acepta varios argumentos en el where, 
+            where : {
+                email: prueba.email,
+            },
+        });
+
+        if (existingUser) {
 
         const canciones= await prisma.song.findMany();
         res.status(200).json({
@@ -31,7 +35,7 @@ export const findAll = async (_req: Request, res: Response): Promise<void> => {
 
         const canciones= await prisma.song.findMany({
             where : {
-                published: false                //publicas; cambiar nombre al campo
+                published: true                //publicas; cambiar nombre al campo
             }
         });
             if (canciones) {
@@ -55,46 +59,53 @@ export const findAll = async (_req: Request, res: Response): Promise<void> => {
         
     
 
-export const findbyid = async (req:Request, res: Response): Promise<void> => {
+export const findbyid = async (_req:Request, res: Response): Promise<void> => {
 
 
-    const token = req.headers["authorization"];
-    const {id} = req.params;
+    const token = _req.headers["authorization"];
+    const {id} = _req.params;
     try {
 
         if (token) {
 
         const prueba =  jwt.verify(token.split(" ")[1], "tokensecret") as Prueba;
         
-        const user = prisma.user.findFirstOrThrow ({
+        const user = await prisma.user.findFirst({
             where: {
                 email: prueba.email,
-                password: prueba.password,
             }
         });
-        const cancionbyid = await prisma.song.findUnique({
-            where: {
-        id: Number(id),
-                },});
-            
-                res.status(500).json({
-                    ok:true,
-                    data: cancionbyid, message: "token valido",
-                })
-            } 
-        else {
+
+        if (user) {
+
             const cancionbyid = await prisma.song.findUnique({
+                where: {
+                    id: Number(id),
+                    },});
+                
+                    res.status(200).json({
+                        ok:true,
+                        data: cancionbyid, message: "token valido",
+                    })
+        } else {res.status(500).json({message: "algo va mal"})}
+        
+        }
+
+        else {
+            const cancionbyid = await prisma.song.findFirst({
                 where: {
             id: Number(id),
                     },});
-            if (cancionbyid?.published == false) {
-                res.json(200).json({data:cancionbyid, message: "cancion publica sin token"})
+
+            if (cancionbyid?.published === true) {
+                res.status(200).json({data:cancionbyid, message: "cancion publica sin token"})
             }
             else {
-            res.status(400).json({message: "cancion privada token no correcto"})}
+            res.status(400).json({message: "cancion privada / token no correcto"})}
             }
 
-        }catch(error) {
+
+        } catch(error) {
             res.status(500).json({message: "error"})}
         };
 
